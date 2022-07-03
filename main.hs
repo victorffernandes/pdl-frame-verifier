b3fcf6d294874ec61ade452ddaaba77308c5e78c
Mauricio <mauricio@bgcbrasil.com.br>
2022-07-03 20:53:43 -0300

    Get relations function

--- main.hs
+++ main.hs
@@ -10,14 +10,29 @@ split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
 inputConverter :: [Char] -> [Char] -> [Char] -> [[[Char]]]
 inputConverter vertex relation pdl = [split ' ' vertex, split ' ' relation, split ' ' pdl]
 
+--getNextState :: Char -> [[Char]] -> [Char]
+--getNextState actualState relations = filter ()
+
+
+listMyRelations :: [[Char]] -> Char -> [[Char]]
+listMyRelations relations vertex = filter (\(c:_) -> c == vertex) relations
+
+getCharInPos :: [[Char]] -> Int -> Char
+getCharInPos list pos = list!!pos!!0
+
+
+
 main = do
     args <- getArgs                  -- IO [String]
     putStrLn "The arguments are:"
     putStrLn (head args)
     putStrLn (args!!1)
     putStrLn (args!!2)
-    print(inputConverter (args!!0) (args!!1) (args!!2))
-    -- inputConverter args!!0 args!!1 args!!2
+    let inputs = ((inputConverter (args!!0) (args!!1) (args!!2)))
+    print (inputs!!0)
+    -- print ((inputs!!0)!!0!!0)
+    let myChar = (getCharInPos (inputs!!0) 0)
+    print(listMyRelations (inputs!!1) myChar)
 
 
 
