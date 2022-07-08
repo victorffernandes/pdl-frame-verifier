90a502bfd0b72ff7756e3e7684595aecb678ff13
Mauricio <mauricio@bgcbrasil.com.br>
2022-07-08 01:19:55 -0300

    Tentei

--- main.hs
+++ main.hs
@@ -1,7 +1,9 @@
 import System.Environment
 import Data.List
+import Data.Maybe
 import GHC.TypeLits (ErrorMessage(Text))
 import Data.Set (foldr)
+import Data.Char
 
 
 listMyRelations :: [[Char]] -> Char -> [[Char]]
@@ -10,26 +12,15 @@ listMyRelations relations vertex = filter (\(c:_) -> c == vertex) relations
 getCharInPos :: [[Char]] -> Int -> Char
 getCharInPos list pos = list!!pos!!0
 
-axiom :: Char -> [[Char]] -> [[Char]]
-axiom state relations = map tail (listMyRelations relations state)
+graphToMatrixAux :: [Char] -> [[Char]] -> [[Int]]
+graphToMatrixAux rel all = map(\x -> relationToNumber all [rel, x]) (listMyRelations all (rel!!1))
 
-sequential :: Char -> [Char] -> [[Char]] -> [[Char]]
--- sequential state pdl relations = foldr (\x y -> f state x relations) ([]) (split ';' pdl)
-sequential state pdl relations = concat (map (\x -> f state x relations) (split ';' pdl))
-
-graphToMatrixAux :: [Char] -> [[Char]] -> [[[Char]]]
-graphToMatrixAux rel all = map(\x -> [rel, x]) (listMyRelations all (rel!!1))
+relationToNumber :: [[Char]] -> [[Char]] -> [Int]
+relationToNumber all cell = [(fromMaybe 0 (findIndex(== cell!!0) all)) + 1, (fromMaybe 0 (findIndex(== cell!!1) all)) + 1]
 
 graphToMatrix :: [[Char]] -> [[Int]]
 graphToMatrix [] = []
--- graphToMatrix graph = foldMap (a -> m) (t a)
-
-f :: Char -> [Char] -> [[Char]] -> [[Char]] -- retorna o erro e os estados possíveis
-f state pdl relations
-  | length pdl == 1 = axiom state relations
-  -- | ';' `elem` pdl = sequential state pdl relations
-  -- | '*' `elem` pdl = sequential state pdl relations
-  | otherwise = [" "]
+graphToMatrix graph = foldMap (\rel -> graphToMatrixAux rel graph) graph
 
 split :: Eq a => a -> [a] -> [[a]]
 split d [] = []
@@ -38,46 +29,37 @@ split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
 inputConverter :: [Char] -> [Char] -> [Char] -> [[[Char]]]
 inputConverter vertex relation pdl = [split ' ' vertex, split ' ' relation, split ' ' pdl]
 
---getNextState :: Char -> [[Char]] -> [Char]
---getNextState actualState relations = filter ()
+isOperator :: [Char] -> Int
+isOperator ";" = 1
+isOperator "*" = 2
+isOperator a = 3
+
+reverseList :: [Int] -> [Int]
+reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
+associateLastNext :: [Char] -> [Char] -> [Int]
+associateLastNext last next
+    | length last == 3 = [digitToInt (last!!2), digitToInt (next!!0)]
+    | length last /= 3 = [digitToInt (last!!0), digitToInt (next!!0)]
 
 
-listMyRelations :: [[Char]] -> Char -> [[Char]]
-listMyRelations relations vertex = filter (\(c:_) -> c == vertex) relations
+pdlToMatrixAux :: Int -> [Char] -> [[Char]] -> [Int]
+pdlToMatrixAux index actual all
+    | length actual == 1 && isOperator(actual) == 3 = [] -- Cenário de programa isolado (1)
+    | length actual == 3 = [digitToInt (actual!!0), digitToInt (actual!!2)] -- Cenário de programas sequenciais (1;3)
+    | length actual == 2 = [digitToInt (actual!!0), digitToInt (actual!!0)] -- Cenário de laço não determinísticos (1*)
+    | length actual == 1 && isOperator(actual) == 2 = reverseList (pdlToMatrixAux (index - 1) (all!!(index -1)) all) -- Cenário de conectivo de laço não determinístico (*)
+    | length actual == 1 && isOperator(actual) == 1 = associateLastNext (all!!(index-1)) (all!!(index+1)) -- Cenário de conectivo sequencial (;)
 
-getCharInPos :: [[Char]] -> Int -> Char
-getCharInPos list pos = list!!pos!!0
-
-
-
-main = do
-    args <- getArgs                  -- IO [String]
-    putStrLn "The arguments are:"
-    let inputs = ((inputConverter (args!!0) (args!!1) (args!!2)))
-    let states = (inputs!!0)
-    print(states)
-    
-
-    concat(map (\x -> f (x) (inputs!!2!!0) (inputs!!1)) (['a', 'b']))
-    print(f (inputs!!0!!0!!0) (inputs!!2!!0) (inputs!!1))
-
-
-
-
-
-
--- graphParser textGraph = 
+pdlToMatrix :: [[Char]] -> [[Int]]
+pdlToMatrix pdl = map(\elem -> pdlToMatrixAux (fromMaybe 0 (findIndex(== elem) pdl)) elem pdl) pdl
 
 
 
 
 
-
--- wordCount input = show (length (lines input)) ++ "\n" ++ show (length (lines input2)) ++ "\n"
--- wordCount2 input2 = show (length (lines input)) ++ "\n"
-
--- main = interact wordCount
 main = do 
-  print (graphToMatrixAux "ac" ["ab", "bc", "cd"])
-  -- print (graphToMatrix "ab" ["ab", "bc", "cd"])
--- main = print(f ("a" (["1"]) (["ab", "bc"])))
+  print (graphToMatrixAux "ab" ["ab", "bc", "cd"])
+  print (graphToMatrix ["ab", "bc", "cd", "bk"])
+  print (pdlToMatrix ["1;2", "*", "3*"])
